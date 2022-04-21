f7aec9da7cbd99d4d6b167aa3bafd1abc28c3cc2
Steven Bennett <steven@budgetbloom.com>
2022-04-20 23:22:48 -0700

    Quick release - patch

---
+++ .srclaunch/config.js
@@ -0,0 +1,22 @@
+import {
+  BuildFormat,
+  BuildPlatform,
+  BuildTarget,
+  BuildTool,
+  ProjectType,
+} from '@srclaunch/types';
+
+export default {
+  name: '@srclaunch/themes',
+  description:
+    'CSS variables and styled-component based themes used by SrcLaunch applications.',
+  type: ProjectType.Library,
+  build: {
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Browser,
+    sourcemap: true,
+    splitting: false,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # @srclaunch/themes
+
+![Issues](https://img.shields.io/github/issues/srclaunch/themes?label=Issues) ![Build](https://github.com/srclaunch/themes/actions/workflows/publish.yml/badge.svg)
+
+CSS variables and styled-component based themes used by SrcLaunch applications.
