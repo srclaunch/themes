1aad85ac3426b1d25faeb528abd7d9a62309716b
Steven Bennett <steven@budgetbloom.com>
2022-04-24 04:20:33 -0700

    Quick release - patch

--- .srclaunch/config.js
+++ .srclaunch/config.js
@@ -4,17 +4,31 @@ import {
   BuildTarget,
   BuildTool,
   ProjectType,
+  TestReporter,
+  TestTool
 } from '@srclaunch/types';
 
 export default {
   name: '@srclaunch/themes',
-  description:
-    'CSS variables and styled-component based themes used by SrcLaunch applications.',
+  description: 'CSS variables and styled-component based themes used by SrcLaunch applications',
   type: ProjectType.Library,
   build: {
+    bundle: {
+      exclude: ['react'],
+    },
     formats: [BuildFormat.ESM, BuildFormat.UMD],
-    platform: BuildPlatform.Browser,
+    platform: BuildPlatform.Node,
     target: BuildTarget.ESNext,
     tool: BuildTool.Vite,
+  },
+  test: {
+    coverage: {
+      reporters: [TestReporter.Lcov, TestReporter.JSONSummary],
+    },
+    files: {
+      include: ['src/**/*.test.{js,jsx,ts,tsx}'],
+    },
+    tool: TestTool.Jest,
+    verbose: true,
   },
 };
---
+++ src/tests/pass.test.ts
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+describe('PassingTest', () => {
+  test('renders without props', () => {
+    expect(true).toBeTruthy();
+  });
+});
